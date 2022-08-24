23f2c09f727b8bd6aaf79f863447df59a852f0f9
Mirleny guerrero <mkathyguerrero@gmail.com>
2022-08-24 10:47:31 -0400

    proyecto maven prueba con funcionalidad de calculadora

---
+++ src/test/java/calculatorTest/calculatorTest.java
@@ -0,0 +1,37 @@
+package calculatorTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import CalculatorProject.Calculator;
+
+public class calculatorTest {
+	Calculator calc;
+	@Before
+	public void setUp() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void addTwoNumbers() {
+		assertEquals(20,calc.add(10, 10));
+	}
+	
+	@Test
+	public void addThreeNumbers() {
+		assertEquals(20,calc.add(1, 9, 10));
+	}
+	
+	@Test
+	public void subtractTwoNumbers() {
+		assertEquals(0,calc.subtract(10, 10));
+	}
+	
+	@Test
+	public void subtractThreeNumbers() {
+		assertEquals(0,calc.subtract(20, 10, 10));
+	}
+	
+}
